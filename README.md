4ecabbab1f0e2279b360b18a3c63d3e0c2533cc5
Robert Diers <robertdiers@gmail.com>
2020-11-09 15:20:42 +0100

    output update

--- README.md
+++ README.md
@@ -8,5 +8,9 @@ adjust execution in ksem_idm_cron file
 
 ```sudo docker build -t ksem_idm:1.0 .```
 
-```sudo docker run ksem_idm:1.0```
+```sudo docker run -d --name ksem_idm ksem_idm:1.0```
+
+```sudo docker logs ksem_idm```
+
+```sudo docker rm -f ksem_idm```
 
--- ksem_idm.sh
+++ ksem_idm.sh
@@ -15,7 +15,7 @@ fed_in_energy=${output_ksem_array[-1]}
 # we need to ignore the value in brackets if it exists
 #echo $fed_in_energy
 if [[ $fed_in_energy == *"("* ]]; then
-  echo "ignoring $fed_in_energy"
+  #echo "ignoring $fed_in_energy"
   fed_in_energy=${output_ksem_array[-2]}
 fi
 
@@ -33,13 +33,13 @@ output_idm=$(mbpoll -0 -r 74 -t 4:float $ip_idm $fed_in_energy)
 #echo $output_idm
 now=$(date +"%d.%m.%Y %T")
 if [[ $output_idm == *"Written 1 references"* ]]; then
-  echo "$now - successfully send $fed_in_energy to $ip_idm"
+  echo "$now - successfully send $fed_in_energy (kW) to iDM $ip_idm"
 else
   echo "$now - failed sending $fed_in_energy to $ip_idm"
   echo $output_idm
 fi
 
-# as GUI in iDM is not showing correct values, we will reed and print
+# now we want to read the updated value
 output_idm=$(mbpoll -0 -r 74 -c 1 -1 -t 4:float $ip_idm)
 
 # extract the actual value
@@ -49,7 +49,7 @@ output_idm_energy=${output_idm_array[-1]}
 
 # print it out
 now=$(date +"%d.%m.%Y %T")
-echo "$now - read value iDM pump (kW): " $output_idm_energy
+echo "$now - read value iDM (kW): " $output_idm_energy
 
 # now we want to read the expexted load
 output_idm=$(mbpoll -0 -r 4122 -c 2 -1 -t 4:float $ip_idm)
@@ -61,7 +61,7 @@ output_idm_energy=${output_idm_array[-1]}
 
 # print it out
 now=$(date +"%d.%m.%Y %T")
-echo "$now - read load iDM pump (kW): " $output_idm_energy
+echo "$now - read load iDM (kW): " $output_idm_energy
 
 
 
